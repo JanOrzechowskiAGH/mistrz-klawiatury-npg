8de2b703268521b0e7b138845ab6f79e0b505933
jochman03 <ochman@student.agh.edu.pl>
2024-05-21 18:03:00 +0200

    Zapis i odczyt dodatkowe funkcjonalności
    Dodano zapis usuniętych przez użytkownika znaków oraz błędów popełnionych w wyrazie (o ile będzie to wykorzystywane). Kod nieco czytelniejszy

--- src/savegame.cpp
+++ src/savegame.cpp
@@ -9,8 +9,8 @@ void save(std::vector<Entry> list, std::string username){
     std::string path = "../saves/" + username + ".savegame";
     file_out.open(path);
     file_out << username << '\n';
-    for (auto element : list){
-        file_out << element.getTime() << " " << element.getWord() << " \n";
+    for (auto& element : list){
+        file_out << element.getTime() << " " << element.getBackspaces() << " " << element.getErrors()<< " " << element.getWord() << " \n";
     }
 
     file_out.close();
@@ -20,43 +20,70 @@ void save(std::vector<Entry> list, std::string username){
 void load(std::vector<Entry>& list, std::string& username, std::string path){
     std::ifstream file_in (path);
     if (file_in.is_open()) {
-        bool data = false;
-        bool name = true;
+        int pos = -1;
         char temp_char;
         double elapsed_time;
+        int errors;
+        int backspaces;
+
         std::string word;
         std::string elapsed_time_s;
-
+        std::string backspaces_s;
+        std::string errors_s;
         while ( file_in ) {
+
             temp_char = file_in.get();
-            if (name) {
-                if (temp_char != '\n') {
+            if (pos == -1) {
+                if (temp_char == '\n') {
+                    pos = 0;
+                }
+                else{
                     username += std::string(1, temp_char);
-                } else {
-                    name = false;
                 }
             }
-            else {
+            else if (pos == 0) {
                 if (temp_char == ' ') {
-                    data = true;
-                } else if (temp_char == '\n') {
-                    data = false;
+                    pos = 1;
                     elapsed_time = std::stod(elapsed_time_s);
+                }
+                else{
+                    elapsed_time_s += std::string(1, temp_char);
+                }
+            }
+            else if(pos == 1){
 
-                    Entry entry(elapsed_time, word);
+                if (temp_char == ' ') {
+                    pos = 2;
+                    backspaces = std::stoi(backspaces_s);
+                }
+                else{
+                    backspaces_s += std::string(1, temp_char);
+                }
+            }
+            else if(pos == 2){
+                if (temp_char == ' ') {
+                    pos = 3;
+                    errors = std::stoi(errors_s);
+                }
+                else{
+                    errors_s += std::string(1, temp_char);
+                }
+            }
+            else{
+                if (temp_char != '\n') {
+                    word += std::string(1, temp_char);
+                } else {
+                    Entry entry(elapsed_time, backspaces, errors, word);
                     list.push_back(entry);
-
                     elapsed_time = 0;
                     elapsed_time_s = "";
+                    errors = 0;
+                    errors_s = "";
+                    backspaces = 0;
+                    backspaces_s = "";
                     word = "";
-                } else {
-                    if (!data) {
-                        elapsed_time_s += std::string(1, temp_char);
-                    } else {
-                        word += std::string(1, temp_char);
-                    }
+                    pos = 0;
                 }
-
             }
         }
 
@@ -64,27 +91,29 @@ void load(std::vector<Entry>& list, std::string& username, std::string path){
 }
 
     /* Przykład zapisu, najlepiej przy każdym zakończonym słowie tworzyć Entry i dodawać do wektora.
-    Entry f1(1.2, "wyindywidualizowany");
-    Entry f2(0.5, "rycerz");
-    Entry f3(0.6, "smok");
-    Entry f4(0.4, "magiczny");
-    Entry f5(0.7, "zamek");
-    Entry f6(0.5, "skarb");
+    Entry f1(1.2, 2, 2, "wyindywidualizowany");
+    Entry f2(0.5, 1, 1, "rycerz");
+    Entry f3(0.6, 4, 5, "smok");
+    Entry f4(0.4, 2, 6, "magiczny");
+    Entry f5(0.7, 8, 9, "zamek");
+    Entry f6(0.5, 10, 8, "skarb");
 
     std::vector<Entry> list = {f1, f2, f3, f4, f5, f6};
 
-    save(list, "Ai");
+    save(list, "ChatGPT");
 
     tu przykład odczytu.
 
     std::vector<Entry> list;
     std::string name;
 
-    load(list, name, "../saves/Ai.savegame");
-    double time_ = 0;
+    load(list, name, "../saves/ChatGPT.savegame");
     for (auto element : list){
-        std::cout << element.getWord() << " " << element.getTime() << " \n";
-        time_ += element.getTime();
+        std::cout << element.getWord() << " ";
+        std::cout << element.getErrors() << " ";
+        std::cout << element.getBackspaces() << " ";
+        std::cout << element.getTime();
+        std::cout << "\n";
     }
     std::cout << name;
     */
--- src/savegame.h
+++ src/savegame.h
@@ -8,13 +8,18 @@
 
 class Entry {
 public:
-    Entry(double elapsed_time, std::string word) : elapsed_time_(elapsed_time), word_(word) {}
+    Entry(double elapsed_time, int backspaces, int errors, std::string word) : elapsed_time_(elapsed_time), backspaces_(backspaces), errors_(errors), word_(word) {}
     double getTime() const { return elapsed_time_; }
+    int getBackspaces() const { return backspaces_; }
+    int getErrors() const { return errors_; }
     std::string getWord() const { return word_; }
 private:
     double elapsed_time_;
+    int backspaces_;
+    int errors_;
     std::string word_;
 };
+
 
 void save(std::vector<Entry> list, std::string username);
 
