515837e39a1132dd05e370e6f833296aed7156f2
Franek Pikon <pikonfranek@student.agh.edu.pl>
2024-05-25 17:43:17 +0200

    Sortowanie statystyk

--- src/stats.cpp
+++ src/stats.cpp
@@ -5,12 +5,13 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <algorithm>
 
 // Ogólne to trzeba zrobiś jakąś listę użytkowników
 // ale to chyba bedzie tworzone w głównym kodzie gry więc na razie to tak zostawiam
 // Tutaj po prostu biorę username, load-uje to co ma zapisane w save-ach i pakuje to do listy którą ty wrzucasz
 
-void get_user_stats(std::vector<StatsEntry>& list, std::string& username){
+void getUserStats(std::vector<StatsEntry>& list, std::string& username){
     std::vector<Entry> temp;
     std::string path = "../saves/" + username + ".savegame";
     load(temp, username, path);
@@ -27,8 +28,38 @@ void get_user_stats(std::vector<StatsEntry>& list, std::string& username){
     list.push_back(userstats);
 }
 
-void get_many_users_stats(std::vector<StatsEntry>& list, std::vector<std::string>& user_list){
+void getMultipeUsersStats(std::vector<StatsEntry>& list, std::vector<std::string>& user_list){
     for (auto& user : user_list){
-        get_user_stats(list, user);
+        getUserStats(list, user);
     }
+}
+
+// Switch nie działa dla std::string więc jest else if
+bool compareByParameter(const StatsEntry& a, const StatsEntry& b, const std::string& parameter) {
+    if (parameter == "Games") {
+        return a.getNumGames() < b.getNumGames();
+    } else if (parameter == "Chars") {
+        return a.getNumChars() < b.getNumChars();
+    } else if (parameter == "Playtime") {
+        return a.getPlaytime() < b.getPlaytime();
+    } else if (parameter == "Backspaces") {
+        return a.getBackspaces() < b.getBackspaces();
+    } else if (parameter == "Errors") {
+        return a.getErrors() < b.getErrors();
+    } else if (parameter == "CharPerS") {
+        return a.getCharPerS() < b.getCharPerS();
+    } else if (parameter == "AvgBackspaces") {
+        return a.getAvgBackspaces() < b.getAvgBackspaces();
+    } else if (parameter == "AvgErrors") {
+        return a.getAvgErrors() < b.getAvgErrors();
+    } else {
+        throw std::invalid_argument("Invalid sorting parameter");
+    }
+
+}
+
+void sortStatsEntries(std::vector<StatsEntry>& stats, const std::string& parameter) {
+    std::sort(stats.begin(), stats.end(), [&parameter](const StatsEntry& a, const StatsEntry& b) {
+        return compareByParameter(a, b, parameter);
+    });
 }
--- src/stats.hpp
+++ src/stats.hpp
@@ -14,7 +14,7 @@ Myślę że wystarczy wyświtlić coś w tym stylu:
 
 class StatsEntry{
 public:
-    StatsEntry(std::string& username, int games = 0, int num_char = 0, double playtime = 0, int backspaces = 0, int errors = 0) :
+    StatsEntry(const std::string& username, int games = 0, int num_char = 0, double playtime = 0, int backspaces = 0, int errors = 0) :
             username_(username), games_(games), char_(num_char), playtime_(playtime), backspaces_(backspaces), errors_(errors) {}
 
     std::string getUsername() const {return username_;}
@@ -24,9 +24,9 @@ public:
     int getBackspaces() const {return backspaces_;}
     int getErrors() const {return errors_;}
 
-    double getCharPerS() const {return (double)char_ / playtime_;}
-    double getAvgBackspaces() const {return (double)backspaces_/games_;}
-    double getAvgErrors() const {return (double)errors_/games_;}
+    double getCharPerS() const {return playtime_ > 0 ? (double)char_ / playtime_: 0;}
+    double getAvgBackspaces() const {return games_ > 0 ? (double)backspaces_/games_: 0;}
+    double getAvgErrors() const {return games_ > 0 ?(double)errors_/games_: 0;}
 
     void add_playtime(double time){playtime_+= time;}
     void add_chars(int number){char_+= number;}
@@ -45,8 +45,13 @@ private:
     double playtime_;
 };
 
-void get_user_stats(std::vector<StatsEntry>& list, std::string& username);
+void getUserStats(std::vector<StatsEntry>& list, std::string& username);
 
-void get_many_users_stats(std::vector<StatsEntry>& list, std::vector<std::string>& user_list);
+void getMultipeUsersStats(std::vector<StatsEntry>& list, std::vector<std::string>& user_list);
+
+bool compareByParameter(const StatsEntry& a, const StatsEntry& b, const std::string& parameter);
+
+void sortStatsEntries(std::vector<StatsEntry>& stats, const std::string& parameter);
+
 
 #endif //NPG_STATS_HPP
