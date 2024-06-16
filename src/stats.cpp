d180646c5e42911b4c7415a4766de46d6243e7c3
Franek Pikon <pikonfranek@student.agh.edu.pl>
2024-06-16 14:43:32 +0200

    Update statystyk

--- src/stats.cpp
+++ src/stats.cpp
@@ -1,66 +1,52 @@
 #include <iostream>
 #include "stats.hpp"
 #include "savegame.h"
-#include <fstream>
 #include <vector>
-#include <string>
 #include <sstream>
 #include <algorithm>
-#include <filesystem>
 
-// Ogólne to trzeba zrobiś jakąś listę użytkowników     <- Funkcja zrobiona, teraz trzeba jakoś ją użyć
-// ale to chyba bedzie tworzone w głównym kodzie gry więc na razie to tak zostawiam
-// Tutaj po prostu biorę username, load-uje to co ma zapisane w save-ach i pakuje to do listy którą ty wrzucasz
-
-void getUserStats(std::vector<StatsEntry>& list, std::string& username){
-    std::vector<Entry> temp;
-    std::string path = "../saves/" + username + ".savegame";
-    load(temp, username, path);
-
-    StatsEntry userstats(username);
-    for (const auto& element : temp){
-        userstats.add_game_number();
-        userstats.add_playtime(element.getTime());
-        userstats.add_backspaces(element.getBackspaces());
-        userstats.add_errors(element.getErrors());
-        userstats.add_chars((int)element.getWord().length());
+int StatsEntry::getAvgScore() const {
+    int sum = 0;
+    for (auto i: score_){
+        sum += i;
     }
-
-    list.push_back(userstats);
+    return games_>0 ? sum/games_ : 0;
 }
 
-void getMultipeUsersStats(std::vector<StatsEntry>& list, std::vector<std::string>& user_list){
-    for (auto& user : user_list){
-        getUserStats(list, user);
+int StatsEntry::getHighScore() const {
+    int highest = 0;
+    for (auto i: score_){
+        if (i>highest){
+            highest = i;
+        }
+    }
+    return highest;
+}
+
+std::vector<StatsEntry> getUsersStats(std::vector<User>& users){
+    std::vector<StatsEntry> users_stats;
+    load(users);
+    for (const auto& user: users){
+        StatsEntry tmp(user.GetUserName(), user.getScore(), user.getGameCount());
+        users_stats.push_back(tmp);
+    }
+    return users_stats;
+}
+
+bool compareStatsEntries(const StatsEntry& a, const StatsEntry& b, Comparator parameter){
+    switch (parameter) {
+        case GameCount:
+            return a.getGameCount() < b.getGameCount();
+        case HighScore:
+            return a.getHighScore() < b.getHighScore();
+        case AvgScore:
+            return a.getAvgScore() < b.getAvgScore();
     }
 }
 
-// Switch nie działa dla std::string więc jest else if
-bool compareByParameter(const StatsEntry& a, const StatsEntry& b, const std::string& parameter) {
-    if (parameter == "Games") {
-        return a.getNumGames() < b.getNumGames();
-    } else if (parameter == "Chars") {
-        return a.getNumChars() < b.getNumChars();
-    } else if (parameter == "Playtime") {
-        return a.getPlaytime() < b.getPlaytime();
-    } else if (parameter == "Backspaces") {
-        return a.getBackspaces() < b.getBackspaces();
-    } else if (parameter == "Errors") {
-        return a.getErrors() < b.getErrors();
-    } else if (parameter == "CharPerS") {
-        return a.getCharPerS() < b.getCharPerS();
-    } else if (parameter == "AvgBackspaces") {
-        return a.getAvgBackspaces() < b.getAvgBackspaces();
-    } else if (parameter == "AvgErrors") {
-        return a.getAvgErrors() < b.getAvgErrors();
-    } else {
-        throw std::invalid_argument("Invalid sorting parameter");
-    }
 
-}
-
-void sortStatsEntries(std::vector<StatsEntry>& stats, const std::string& parameter) {
+void sortStatsEntries(std::vector<StatsEntry>& stats, Comparator parameter){
     std::sort(stats.begin(), stats.end(), [&parameter](const StatsEntry& a, const StatsEntry& b) {
-        return compareByParameter(a, b, parameter);
+        return compareStatsEntries(a, b, parameter);
     });
 }
--- src/stats.hpp
+++ src/stats.hpp
@@ -1,55 +1,40 @@
 #ifndef NPG_STATS_HPP
 #define NPG_STATS_HPP
 
-#include <cstdlib>
 #include <string>
 #include <utility>
 #include <vector>
-#include <fstream>
-/*
-No mam nadzieje że to działa xdd
-Myślę że wystarczy wyświtlić coś w tym stylu:
- Userame Num_of_games Play_Time Char_per_s Avg_Errors
-*/
+
+enum Comparator {
+    GameCount,
+    HighScore,
+    AvgScore
+};
 
 class StatsEntry{
 public:
-    StatsEntry(const std::string& username, int games = 0, int num_char = 0, double playtime = 0, int backspaces = 0, int errors = 0) :
-            username_(username), games_(games), char_(num_char), playtime_(playtime), backspaces_(backspaces), errors_(errors) {}
+    StatsEntry(std::string  username, std::vector<int> score,int games = 0) :
+            username_(std::move(username)), games_(games), score_(score) {}
 
     std::string getUsername() const {return username_;}
-    int getNumGames() const {return games_;}
-    int getNumChars() const {return char_;}
-    double getPlaytime() const {return playtime_;}
-    int getBackspaces() const {return backspaces_;}
-    int getErrors() const {return errors_;}
-
-    double getCharPerS() const {return playtime_ > 0 ? (double)char_ / playtime_: 0;}
-    double getAvgBackspaces() const {return games_ > 0 ? (double)backspaces_/games_: 0;}
-    double getAvgErrors() const {return games_ > 0 ?(double)errors_/games_: 0;}
-
-    void add_playtime(double time){playtime_+= time;}
-    void add_chars(int number){char_+= number;}
-    void add_game_number(){games_++;}
-    void add_backspaces(int number){backspaces_+= number;}
-    void add_errors(int number){errors_+= number;}
-
-
+    std::vector<int> getScore() const { return score_; }
+    int getGameCount() const {return games_;}
+    int getHighScore() const;
+    int getAvgScore() const;
 
 private:
     std::string username_;
+    std::vector<int> score_;
     int games_;
-    int char_;
-    int backspaces_;
-    int errors_;
-    double playtime_;
 };
 
-void getUserStats(std::vector<StatsEntry>& list, std::string& username);
+StatsEntry getUsersStats(std::string& username);
 
-void getMultipeUsersStats(std::vector<StatsEntry>& list, std::vector<std::string>& user_list);
+std::vector<StatsEntry> getMultipleUsersStats(std::vector<std::string>& user_list);
 
-bool compareByParameter(const StatsEntry& a, const StatsEntry& b, const std::string& parameter);
+bool compareStatsEntries(const StatsEntry& a, const StatsEntry& b, Comparator parameter);
 
-void sortStatsEntries(std::vector<StatsEntry>& stats, const std::string& parameter);
+void sortStatsEntries(std::vector<StatsEntry>& stats, Comparator parameter);
+
+
 #endif //NPG_STATS_HPP
