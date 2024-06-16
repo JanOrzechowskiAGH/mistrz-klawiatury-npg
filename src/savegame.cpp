19aa4d9c8d86f4c435305668225cc3a6bdefdb0b
jochman03 <ochman@student.agh.edu.pl>
2024-06-16 13:04:59 +0200

    Optymalizacja zapisu #3

--- src/savegame.cpp
+++ src/savegame.cpp
@@ -5,26 +5,40 @@
 #include <fstream>
 #include <filesystem>
 
-void GetUserList(std::vector<std::string>& list){
+void load(std::vector<User>& users){
     std::string path = "../saves/";
     for (const auto & file : std::filesystem::directory_iterator(path)) {
         std::ifstream file_in (file.path());
         char temp_char;
         std::string username;
+        std::string score_s;
+        int score;
+        int line = 0;
         while ( file_in ) {
             temp_char =  (char) file_in.get();
-            if (temp_char == '\n') {
-                list.push_back(username);
-                break;
+            if(line == 0) {
+                if (temp_char == '\n') {
+                    line++;
+                } else {
+                    username += std::string(1, temp_char);
+                }
             }
             else{
-                username += std::string(1, temp_char);
+                if (temp_char == '\n') {
+                    score = std::stoi(score_s);
+                    User new_user = User(username, score);
+                    users.push_back(new_user);
+                    username = "";
+                    score = 0;
+                } else {
+                    score_s += std::string(1, temp_char);
+                }
             }
         }
         file_in.close();
     }
 }
-void save(std::string username, int score){
+void save(std::string& username, int& score){
     std::filesystem::create_directory("../saves");
     std::string path = "../saves/" + username + ".savegame";
     std::ofstream file_out;
--- src/savegame.h
+++ src/savegame.h
@@ -6,8 +6,18 @@
 #include <vector>
 #include <fstream>
 
-void GetUserList(std::vector<std::string>& list);
+class User{
+public:
+    User(std::string UserName, int score) : score_(score), UserName_(std::move(UserName)) {}
+    int getScore() const { return score_; }
+    std::string GetUserName () const { return UserName_; }
+private:
+    int score_;
+    std::string UserName_;
+};
 
-void save(std::string username, double time,  int score1, int score2);
+void save(std::string& username, int& score);
+
+void load(std::vector<User>& users);
 
 #endif //SAVEGAME_H
